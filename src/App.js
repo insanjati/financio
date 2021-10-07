ee3e7194ffa908e18cd88b9abc4c086dc16314f5
Insan Jati <insan.jati@tokopedia.com>
2021-10-07 22:21:35 +0700

    First commit of Financio

--- src/App.js
+++ src/App.js
@@ -7,8 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Financio!
         </p>
+        <small>
+         Write down you expenses and let us do the math.
+        </small>
         <a
           className="App-link"
           href="https://reactjs.org"
